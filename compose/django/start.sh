2da1557f604e301c860e1c7d4894080b1ce3990b
osvaldo hinojosa <osvaldo.javier14@gmail.com>
2023-10-31 22:36:53 -0600

    feat: base files

---
+++ compose/django/start.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+python manage.py makemigrations
+python manage.py migrate
+python manage.py collectstatic --noinput
+python manage.py runserver 0.0.0.0:8000
