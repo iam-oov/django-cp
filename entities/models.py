a247d9f7fe0f612bb0fa9a34045d41d8cfdb2500
osvaldo hinojosa <osvaldo.javier14@gmail.com>
2023-10-31 23:44:13 -0600

    feat: add entity models

--- entities/models.py
+++ entities/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+
+class FederalEntity(models.Model):
+    slug = models.SlugField(max_length=30)
+    key = models.PositiveSmallIntegerField()
+    name = models.CharField(max_length=200)
+    code = models.CharField(max_length=200)
+
+    def __str__(self):
+        return "{0}: {1}".format(self.slug, self.name)
+
+
+class Municipality(models.Model):
+    slug = models.SlugField(max_length=30)
+    key = models.PositiveSmallIntegerField()
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return "{0}: {1}".format(self.slug, self.name)
+
+
+class Settlement(models.Model):
+    slug = models.SlugField(max_length=30)
+    key = models.PositiveSmallIntegerField()
+    name = models.CharField(max_length=200)
+    zone_type = models.CharField(max_length=200)
+    type = models.CharField(max_length=200)
+
+    def __str__(self):
+        return "{0}: {1}".format(self.slug, self.name)
+
+
+class Entity(models.Model):
+    zip_code = models.PositiveIntegerField()
+    locality = models.CharField(max_length=400)  # ciudad
