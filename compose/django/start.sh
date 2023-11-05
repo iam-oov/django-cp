9c1a547b6bcdce65dbfe86e6abf4f3fb04777daa
osvaldo hinojosa <osvaldo.javier14@gmail.com>
2023-11-05 10:41:46 -0600

    feat: primeros pasos de celery-beat

--- compose/django/start.sh
+++ compose/django/start.sh
@@ -4,8 +4,16 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
+wait-for-it my-mysql:3007
+
 python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --noinput
+
+python manage.py createsuperuserwithpassword \
+	--username root \
+	--password 1234abcd \
+	--email admin@mail.org \
+	--preserve
 
 python manage.py runserver 0.0.0.0:8000
--- setup/settings.py
+++ setup/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'entities'
+    'django_celery_beat',
+    'entities',
 ]
 
 MIDDLEWARE = [
